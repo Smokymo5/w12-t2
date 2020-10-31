2b94163b7468990d25a09f25c9075f22847734b4
mouse <krugentul@gmail.com>
2020-10-31 19:22:44 +0700

    changes in dashboard

--- client/components/dashboard.js
+++ client/components/dashboard.js
@@ -6,22 +6,22 @@ const Dashboard = () => {
   return (
     <div>
       <Head title="Hello" />
-      {/* <div className="flex items-center justify-center h-screen"> */}
-      <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
-        <div id="title">
-          Dashboard
-          <div>
-            <Link to="/dashboard/profile/b32f67d5-107c-465b-b6af-ae8194b0e805">
-              {' '}
-              Go To Profile{' '}
-            </Link>
-          </div>
-          <div>
-            <Link to="/dashboard/main"> Go To Main </Link>
+      <div className="flex items-center justify-center h-screen">
+        <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
+          <div id="title">
+            dashboard
+            <div>
+              <Link to="/dashboard/profile/b32f67d5-107c-465b-b6af-ae8194b0e805">
+                {' '}
+                Go To Profile{' '}
+              </Link>
+            </div>
+            <div>
+              <Link to="/dashboard/main"> Go To Main </Link>
+            </div>
           </div>
         </div>
       </div>
-      {/* </div> */}
     </div>
   )
 }
