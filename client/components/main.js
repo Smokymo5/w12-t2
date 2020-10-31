c268b090c96a805d4209abb8a962ba03033ba7f7
mouse <krugentul@gmail.com>
2020-10-31 19:58:29 +0700

    changes in dashboard, main, profile

--- client/components/main.js
+++ client/components/main.js
@@ -8,17 +8,12 @@ const Main = () => {
       <Head title="Hello" />
       {/* <div className="flex items-center justify-center h-screen"> */}
       <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
-        <div id="title">
-          Main
-          <div>
-            <Link to="/dashboard/profile/b32f67d5-107c-465b-b6af-ae8194b0e805">
-              {' '}
-              Go To Profile{' '}
-            </Link>
-          </div>
-          <div>
-            <Link to="/dashboard"> Go To Root </Link>
-          </div>
+        <div id="title">Main</div>
+        <div>
+          <Link to="/dashboard/profile/b32f67d5-107c-465b-b6af-ae8194b0e805"> Go To Profile </Link>
+        </div>
+        <div>
+          <Link to="/dashboard"> Go To Root </Link>
         </div>
       </div>
       {/* </div> */}
--- client/components/profile.js
+++ client/components/profile.js
@@ -9,18 +9,16 @@ const Profile = () => {
       <Head title="Hello" />
       {/* <div className="flex items-center justify-center h-screen"> */}
       <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
-        <div id="title">
-          Profile
-          <div>
-            <Link to="/dashboard"> Go To Root </Link>
-          </div>
-          <div>
-            <Link to="/dashboard/main"> Go To Main </Link>
-          </div>
-          <div id="username"> {user} </div>
-        </div>
+        <div id="title">Profile</div>
       </div>
       {/* </div> */}
+      <div>
+        <Link to="/dashboard"> Go To Root </Link>
+      </div>
+      <div>
+        <Link to="/dashboard/main"> Go To Main </Link>
+      </div>
+      <div id="username"> {user} </div>
     </div>
   )
 }
